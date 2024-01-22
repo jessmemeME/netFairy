35b01f413ba534f9879310978ef3f41a2310ecdc
jessmemeME <jdprincesita@gmail.com>
2024-01-22 13:42:23 -0300

    Se terminaron de crear las clases y modelos de Auth y Accounts
    Se terminaron de crear las clases y modelos de Auth y Accounts

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -130,6 +130,375 @@ namespace FairyBE.Controllers
                 throw ex;
                 
             }
-        }    
+        }
+
+
+
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //************************* AuthGroupPermissions *************************
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //ENDPOINT PARA CREAR UN NUEVO REGISTRO
+
+        [HttpPost("RegisterAuthGroupPermissions")]
+        public async Task<IActionResult> RegisterAuthGroupPermissionsAsync([FromBody] AuthGroupPermissions auth_group_permissions)
+        {
+            int result = -1;
+            string insertQuery = "INSERT INTO auth_group_permissions (id, group_id, permission_id) VALUES (@id, @group_id, @permission_id) RETURNING Id";
+            var queryArguments = new
+            {
+                id = auth_group_permissions.id,
+                group_id = auth_group_permissions.group_id,
+                permission_id = auth_group_permissions.permission_id
+            };
+            try
+            {
+                connection.Open();
+                result = await connection.ExecuteAsync(insertQuery, queryArguments);
+                connection.Close();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw ex;
+            }
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //ENDPOINT PARA EDITAR UN  REGISTRO
+
+        [HttpPost("UpdateAuthGroupPermissions")]
+        public async Task<IActionResult> UpdateAuthGroupPermissions([FromBody] AuthGroupPermissions auth_group_permissions)
+        {
+
+            int result = -1;
+            string insertQuery = "UPDATE auth_group_permissions  SET  id=@, group_id=@, permission_id=@id=@id, group_id=@group_id, permission_id=@permission_id WHERE id = @id"; var queryArguments = new
+            {
+                id = auth_group_permissions.id,
+                group_id = auth_group_permissions.group_id,
+                permission_id = auth_group_permissions.permission_id
+            };
+            try
+            {
+                connection.Open();
+                result = await connection.ExecuteAsync(insertQuery, queryArguments);
+                connection.Close();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw ex;
+            }
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //ENDPOINT PARA ELIMINAR UN  REGISTRO
+
+        [HttpPost("DeleteAuthGroupPermissions")]
+        public async Task<IActionResult> DeleteAuthGroupPermissions([FromBody] AuthGroupPermissions auth_group_permissions)
+        {
+
+            int result = -1;
+            string insertQuery = "DELETE FROM auth_group_permissions WHERE id = @id";
+            var queryArguments = new
+            {
+                id = auth_group_permissions.id,
+                group_id = auth_group_permissions.group_id,
+                permission_id = auth_group_permissions.permission_id
+            };
+            try
+            {
+                connection.Open();
+                result = await connection.ExecuteAsync(insertQuery, queryArguments);
+                connection.Close();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw ex;
+            }
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------       
+        //ENDPOINT PARA LISTAR TODOS LOS REGISTROS DE LA TABLA
+
+        [HttpGet("ListAllAuthGroupPermissionss")]
+        public async Task<IActionResult> ListAllAuthGroupPermissionss()
+        {
+
+            try
+            {
+                string commandText = "SELECT * FROM   auth_group_permissions";
+                connection.Open();
+                var groups = await connection.QueryAsync<AuthGroupPermissions>(commandText);
+                connection.Close();
+                return Ok(groups);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw ex;
+
+            }
+        }
+
+
+
+
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //************************* AuthPermissions *************************
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        //ENDPOINT PARA CREAR UN NUEVO REGISTRO
+
+            [HttpPost("RegisterAuthPermissions")]
+            public async Task<IActionResult> RegisterAuthPermissionsAsync([FromBody] AuthPermissions auth_permission)
+            {
+                int result = -1;
+                string insertQuery = "INSERT INTO auth_permission (id, name, content_type_id, codename) VALUES (@id, @name, @content_type_id, @codename) RETURNING Id";
+                var queryArguments = new
+                {
+                    id = auth_permission.id,
+                    name = auth_permission.name,
+                    content_type_id = auth_permission.content_type_id,
+                    codename = auth_permission.codename
+                };
+                try
+                {
+                    connection.Open();
+                    result = await connection.ExecuteAsync(insertQuery, queryArguments);
+                    connection.Close();
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                    throw ex;
+                }
+            }
+
+            //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //ENDPOINT PARA EDITAR UN  REGISTRO
+
+            [HttpPost("UpdateAuthPermissions")]
+            public async Task<IActionResult> UpdateAuthPermissions([FromBody] AuthPermissions auth_permission)
+            {
+
+                int result = -1;
+                string insertQuery = "UPDATE auth_permission  SET  id=@id, name=@name, content_type_id=@content_type_id, codename=@codename WHERE id = @id"; var queryArguments = new
+                {
+                    id = auth_permission.id,
+                    name = auth_permission.name,
+                    content_type_id = auth_permission.content_type_id,
+                    codename = auth_permission.codename
+                };
+                try
+                {
+                    connection.Open();
+                    result = await connection.ExecuteAsync(insertQuery, queryArguments);
+                    connection.Close();
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                    throw ex;
+                }
+            }
+
+            //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //ENDPOINT PARA ELIMINAR UN  REGISTRO
+
+            [HttpPost("DeleteAuthPermissions")]
+            public async Task<IActionResult> DeleteAuthPermissions([FromBody] AuthPermissions auth_permission)
+            {
+
+                int result = -1;
+                string insertQuery = "DELETE FROM auth_permission WHERE id = @id";
+                var queryArguments = new
+                {
+                    id = auth_permission.id,
+                    name = auth_permission.name,
+                    content_type_id = auth_permission.content_type_id,
+                    codename = auth_permission.codename
+                };
+                try
+                {
+                    connection.Open();
+                    result = await connection.ExecuteAsync(insertQuery, queryArguments);
+                    connection.Close();
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                    throw ex;
+                }
+            }
+
+            //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------       
+            //ENDPOINT PARA LISTAR TODOS LOS REGISTROS DE LA TABLA
+
+            [HttpGet("ListAllAuthPermissionss")]
+            public async Task<IActionResult> ListAllAuthPermissionss()
+            {
+
+                try
+                {
+                    string commandText = "SELECT * FROM   auth_permission";
+                    connection.Open();
+                    var groups = await connection.QueryAsync<AuthPermissions>(commandText);
+                    connection.Close();
+                    return Ok(groups);
+
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                    throw ex;
+
+                }
+            }
+
+
+
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //************************* AuthContentType *************************
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //ENDPOINT PARA CREAR UN NUEVO REGISTRO
+
+        [HttpPost("RegisterAuthContentType")]
+        public async Task<IActionResult> RegisterAuthContentTypeAsync([FromBody] AuthContentType auth_content_type)
+        {
+            int result = -1;
+            string insertQuery = "INSERT INTO auth_content_type (id, app_label, model) VALUES (@id,@app_label,@model) RETURNING Id";
+            var queryArguments = new
+            {
+                id = auth_content_type.id,
+                app_label = auth_content_type.app_label,
+                model = auth_content_type.model
+            };
+            try
+            {
+                connection.Open();
+                result = await connection.ExecuteAsync(insertQuery, queryArguments);
+                connection.Close();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw ex;
+            }
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //ENDPOINT PARA EDITAR UN  REGISTRO
+
+        [HttpPost("UpdateAuthContentType")]
+        public async Task<IActionResult> UpdateAuthContentType([FromBody] AuthContentType auth_content_type)
+        {
+
+            int result = -1;
+            string insertQuery = "UPDATE auth_content_type  SET  app_label=@app_label, model=@model WHERE id = @id"; var queryArguments = new
+            {
+                id = auth_content_type.id,
+                app_label = auth_content_type.app_label,
+                model = auth_content_type.model
+            };
+            try
+            {
+                connection.Open();
+                result = await connection.ExecuteAsync(insertQuery, queryArguments);
+                connection.Close();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw ex;
+            }
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //ENDPOINT PARA ELIMINAR UN  REGISTRO
+
+        [HttpPost("DeleteAuthContentType")]
+        public async Task<IActionResult> DeleteAuthContentType([FromBody] AuthContentType auth_content_type)
+        {
+
+            int result = -1;
+            string insertQuery = "DELETE FROM auth_content_type WHERE id = @id";
+            var queryArguments = new
+            {
+                id = auth_content_type.id,
+                app_label = auth_content_type.app_label,
+                model = auth_content_type.model
+            };
+            try
+            {
+                connection.Open();
+                result = await connection.ExecuteAsync(insertQuery, queryArguments);
+                connection.Close();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw ex;
+            }
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------       
+        //ENDPOINT PARA LISTAR TODOS LOS REGISTROS DE LA TABLA
+
+        [HttpGet("ListAllAuthContentTypes")]
+        public async Task<IActionResult> ListAllAuthContentTypes()
+        {
+
+            try
+            {
+                string commandText = "SELECT * FROM   auth_content_type";
+                connection.Open();
+                var groups = await connection.QueryAsync<AuthContentType>(commandText);
+                connection.Close();
+                return Ok(groups);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw ex;
+
+            }
+        }
+
+
+
+
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
     }
 }
--- Models/Auth.cs
+++ Models/Auth.cs
@@ -4,6 +4,8 @@ la sintaxis es clase, tipo de atributo, nombre del atributo, metodos o acciones 
 = public
 dentro se tienen los atributos */
 
+using System.ComponentModel.DataAnnotations;
+
 namespace FairyBE.Models
 {
     public class Auth //clase
@@ -17,12 +19,49 @@ namespace FairyBE.Models
 
     public class AuthGroupPermissions
     {
+        public int id { get; set; }
+        [Required] public bool group_id { get; set; }
+        [Required] public bool permission_id { get; set; }
 
+        /*
+            "id"	"bigint"
+            "group_id"	"integer"
+            "permission_id"	"integer"
+         TABLE_NAME = 'auth_group_permissions'
+         */
     }
 
     public class AuthPermissions
     {
+        public int id { get; set; }
+        public required string name { get; set; }
+        public required int content_type_id { get; set; }
+        public required string codename { get; set; }
 
+
+        /*
+            "id"	"integer"
+            "name"	"character varying"
+            "content_type_id"	"integer"
+            "codename"	"character varying"
+        TABLE_NAME = 'auth_permission'
+         */
+    }
+    
+
+    public class AuthContentType
+    {
+        public int id { get; set; }
+        public required string app_label { get; set; }
+        public required string model { get; set; }
+
+
+        /*
+        "id"	"integer"
+        "app_label"	"character varying"
+        "model"	"character varying"
+        TABLE_NAME = 'auth_content_type'
+         */
     }
 
 
