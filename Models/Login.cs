0eff180cd5d82c99271aaae3b8b73555a65bd306
alvarortizd <a.ortizd93@gmail.com>
2024-02-25 19:49:57 -0300

    Se agrega el controllador de login

---
+++ Models/Login.cs
@@ -0,0 +1,16 @@
+﻿namespace FairyBE.Models
+{
+    public class Login
+    {
+        public required string email { get; set; }
+        public required string password { get; set; }
+    }
+    public class ResponseLogin {
+        public required string auth_login { get; set; }
+    }
+
+    public class ReturnLogin {
+        public required string  mensaje { get; set; }
+        public required string respuesta { get; set; }
+    }
+}
